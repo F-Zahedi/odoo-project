5074934b6b95f29c339cf5112163ef304f8864d6
F-Zahedi <160347284+F-Zahedi@users.noreply.github.com>
2024-07-02 20:09:37 +0330

    Add files via upload
    uploading odooSample folder

---
+++ OdooSample/forms.py
@@ -0,0 +1,138 @@
+from django import forms
+from .models import *
+
+class LoginForm(forms.Form):
+    user_name = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'نام کاربری خود را وارد نمایید', 'class': 'form-control valid'}),
+        label="نام کاربری"
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'رمز عبور خود را وارد نمایید', 'class': 'form-control valid'}),
+        label="رمز عبور",
+       
+    )
+
+class CreateProductForm(forms.ModelForm):
+    Name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'placeholder': 'نام محصول را وارد کنید', 'class': 'form-control'}
+    ))
+
+    FeePrice = forms.IntegerField(
+        widget=forms.NumberInput(
+            attrs={'placeholder': 'قیمت محصول را وارد کنید', 'class': 'form-control'}
+    ))
+
+    Category = forms.ModelChoiceField(
+        queryset=Category.objects.all(), 
+        widget=forms.Select(
+            attrs={'class': 'form-control'}
+    ))
+
+    Supplier = forms.ModelChoiceField(
+        queryset=Supplier.objects.all(), 
+        widget=forms.Select(
+            attrs={'class': 'form-control'}
+    ))
+
+    class Meta:
+        model = Product
+        fields = ['Name', 'FeePrice']
+
+
+class CreateCategoryForm(forms.ModelForm):
+    Name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'placeholder': 'نام دسته بندی را وارد کنید', 'class': 'form-control'}
+    ))
+
+    Details = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'placeholder': 'توضیحات دسته بندی را وارد کنید', 'class': 'form-control'}
+    ))
+
+   
+    class Meta:
+        model = Category
+        fields = ['Name', 'Details']
+        
+        
+class CreateEmployeeForm(forms.ModelForm):
+    FirstName = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'placeholder': 'نام  را وارد کنید', 'class': 'form-control'}
+    ))
+
+    LastName = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'placeholder': 'نام خانوادگی را وارد کنید', 'class': 'form-control'}
+    ))
+    PhoneNumber = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'placeholder': 'شماره تلفن را وارد کنید', 'class': 'form-control'}
+    ))
+   
+    class Meta:
+        model = Employee
+        fields = ['FirstName', 'LastName', 'PhoneNumber']        
+       
+        
+class CreateCustomerForm(forms.ModelForm):
+    FirstName = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'placeholder': 'نام  را وارد کنید', 'class': 'form-control'}
+    ))
+
+    LastName = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'placeholder': 'نام خانوادگی را وارد کنید', 'class': 'form-control'}
+    ))
+    PhoneNumber = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'placeholder': 'شماره تلفن را وارد کنید', 'class': 'form-control'}
+    ))
+   
+    class Meta:
+        model = Customer
+        fields = ['FirstName', 'LastName', 'PhoneNumber']        
+        
+
+class CreateSupplierForm(forms.ModelForm):
+    Name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'placeholder': 'نام  را وارد کنید', 'class': 'form-control'}
+    ))
+
+    Countery = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'placeholder': 'کشور را وارد کنید', 'class': 'form-control'}
+    ))
+    ContactNumber = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'placeholder': 'شماره تماس را وارد کنید', 'class': 'form-control'}
+    ))
+   
+    class Meta:
+        model = Employee
+        fields = ['Name', 'Countery', 'ContactNumber']
+        
+        
+class CreateDeliverForm(forms.ModelForm):
+    Name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'placeholder': 'نام  را وارد کنید', 'class': 'form-control'}
+    ))
+
+    ContactNumber = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'placeholder': 'شماره تماس را وارد کنید', 'class': 'form-control'}
+    ))
+    DeliverType = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'placeholder': 'نوع ارسال را وارد کنید', 'class': 'form-control'}
+    ))
+   
+    class Meta:
+        model = Deliver
+        fields = ['Name', 'ContactNumber', 'DeliverType']
---
+++ OdooSample/views.py
@@ -0,0 +1,235 @@
+from django.shortcuts import render
+from django.views.generic import *
+from .models import *
+from .forms import *
+from django.db.models import Sum,Count
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, get_user_model, authenticate, logout
+
+
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('/home')
+    login_form = LoginForm(request.POST or None)
+    context = {
+        "login_form": login_form
+    }
+    if login_form.is_valid():
+        user_name = login_form.cleaned_data.get("user_name")
+        password = login_form.cleaned_data.get("password")
+        user = authenticate(request, username=user_name, password=password)
+        if user is not None:
+            login(request, user)
+            context["login_form"] = LoginForm()
+            return redirect('/')
+        else:
+            login_form.add_error('user_name', 'کاربری با مشخصات وارد شده یافت نشد')
+
+    return render(request, 'login.html', context)
+
+def Index(request):
+
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    delivers = Deliver.objects.all()
+    customers = Customer.objects.all()
+    employees = Employee.objects.all()
+    suppliers = Supplier.objects.all()
+    orders = Order.objects.all()
+    orderDetails = OrderDetail.objects.all()
+    productSum = products.aggregate(Sum('FeePrice'))['FeePrice__sum']
+    orderAvg = orderDetails.count() / orders.count()
+    # ordersSum = orderDetails.annotate(
+    # result=F('FeePrice') * F('Count')).first()
+    
+    return render(request,'index.html',context={
+        'productCount':products.count(),
+        'products':products,
+        'categoryCount':categories.count(),
+        'categories':categories,
+        'employeeCount' : employees.count(),
+        'emoloyees': employees,
+        'customerCount' : customers.count(),
+        'customers': customers,
+        'deliverCount' : delivers.count(),
+        'delivers': delivers,
+        'suppliers':suppliers,
+        'supplierCount':suppliers.count(),
+        'orderCount':orders.count(),
+        'orders':orders,
+        'productSum':productSum,
+        'orderAvg':orderAvg
+    })
+
+
+
+class ProductListView(ListView):
+    model = Product
+    
+class CategoryListView(ListView):
+    model = Category
+    
+class OrderListView(ListView):
+    model = Order
+    
+class OrderDetailListView(ListView):
+    model = OrderDetail
+    
+class EmployeeListView(ListView):
+    model = Employee
+    
+class CustomerListView(ListView):
+    model = Customer
+    
+class DeliverListView(ListView):
+    model = Deliver
+    
+class SupplierListView(ListView):
+    model = Supplier
+    
+
+class CreateProduct(CreateView):
+    model = Product
+    template_name = 'product/add_Product.html'
+    form_class = CreateProductForm
+    success_url = '/proudcts'
+  
+class CreateCategory(CreateView):
+    model = Category
+    template_name = 'category/add_Category.html'
+    form_class = CreateCategoryForm
+    success_url = '/categories'
+
+class CreateSupplier(CreateView):
+    model = Supplier
+    template_name = 'supplier/add_Supplier.html'
+    form_class = CreateSupplierForm
+    success_url = '/suppliers'
+    
+class CreateDeliver(CreateView):
+    model = Deliver
+    template_name = 'deliver/add_Deliver.html'
+    form_class = CreateDeliverForm
+    success_url = '/delivers'
+
+class CreateCustomer(CreateView):
+    model = Customer
+    template_name = 'customer/add_Customer.html'
+    form_class = CreateCustomerForm
+    success_url = '/customers'
+    
+class CreateEmployee(CreateView):
+    model = Employee
+    template_name = 'employee/add_Employee.html'
+    form_class = CreateEmployeeForm
+    success_url = '/empolyees'
+
+
+class CategoryUpdate(UpdateView):
+    model = Category
+    fields = ["Name","Details"]
+    template_name = "category/update_Category.html"
+    success_url = '/categories'
+
+class EmployeeUpdate(UpdateView):
+    model = Employee
+    fields = ["FirstName","LastName",'PhoneNumber']
+    template_name = "employee/update_Employee.html"
+    success_url = '/employees'
+
+class CustomerUpdate(UpdateView):
+    model = Customer
+    form_class = CreateCustomerForm
+    # fields = ["FirstName","LastName",'PhoneNumber']
+    template_name = "customer/update_Customer.html"
+    success_url = '/customers'
+
+class SupplierUpdate(UpdateView):
+    model = Supplier
+    fields = ["Name","Details"]
+    template_name = "supplier/update_Supplier.html"
+    success_url = '/suppliers'
+    
+class DeliverUpdate(UpdateView):
+    model = Deliver
+    fields = ["Name","Details"]
+    template_name = "deliver/update_Deliver.html"
+    success_url = '/delivers'
+
+class ProductUpdate(UpdateView):
+    model = Product
+    fields = ["Name","Details"]
+    template_name = "product/update_Product.html"
+    success_url = '/products'
+
+class OrderDetailUpdate(UpdateView):
+    model = OrderDetail
+    fields = []
+    template_name = ""
+    success_url = ""
+
+
+class CategoryDelete(DeleteView):
+    model = Category
+    template_name = "category/delete_Category.html"
+    success_url = '/categories'
+    
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = "product/delete_Product.html"
+    success_url = '/proudcts'
+    
+class CustomerDelete(DeleteView):
+    model = Customer
+    template_name = "customer/delete_Customer.html"
+    success_url = '/customer'
+
+class EmployeeDelete(DeleteView):
+    model = Employee
+    template_name = "employee/delete_Employee.html"
+    success_url = '/employees'
+
+class SupplierDelete(DeleteView):
+    model = Supplier
+    template_name = "supplier/delete_Supplier.html"
+    success_url = '/suppliers'
+    
+class DeliverDelete(DeleteView):
+    model = Deliver
+    template_name = "deliver/delete_Deliver.html"
+    success_url = '/delivers'
+    
+class OrderDelete(DeleteView):
+    model = Order
+    template_name = "order/delete_Order.html"
+    success_url = '/orders'
+    
+class OrderDetailDelete(DeleteView):
+    model = OrderDetail
+    template_name = "orderDetail/delete_OrderDetail.html"
+    success_url = '/orderDetails'
+    
+
+class ProductDetail(DetailView):
+    model = Product
+
+class CategoryDetail(DetailView):
+    model = Category
+
+class SupplierDetail(DetailView):
+    model = Supplier
+
+class DeliverDetail(DetailView):
+    model = Deliver
+
+class EmployeeDetail(DetailView):
+    model = Employee
+
+class CustomerDetail(DetailView):
+    model = Customer
+
+
+
+
+    
